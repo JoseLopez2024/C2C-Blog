541e0806897178937614584ab2fd7c8cd92f3f9c
JoseLopez2024 <Emanuelopez625@gmail.com>
2025-05-13 17:14:04 -0500

    May 13

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,17 +1,24 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/layout';
+import Script from 'next/script';
  
 export default function FirstPost() {
   return (
-    <Layout>
+    <>
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
-        <Link href="/">← Back to home</Link>
+        <Link href="/">Back to home</Link>
       </h2>
-    </Layout>
+    </>
   );
 }
